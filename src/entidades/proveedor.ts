d6bc0e3b0d8f4f716b47e804dc30e843713f0df8
Gonzalo <Fernandez-gonza@hotmail.com>
2023-10-25 17:40:24 -0300

    wip

---
+++ src/entidades/proveedor.ts
@@ -0,0 +1,13 @@
+export class Proveedor {
+    constructor(public id: number, public nombre: string, public teléfono: string) { }
+
+    modificarNombre(nuevoNombre: string) {
+        this.nombre = nuevoNombre;
+    }
+
+    modificarTeléfono(nuevoTeléfono: string) {
+        this.teléfono = nuevoTeléfono;
+    }
+}
+
+
